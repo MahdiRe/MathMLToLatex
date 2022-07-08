9020a4aec8d98e78850118bf1c79899053c0b0a6
MahdiRe <mahdhi.dts@gmail.com>
2022-07-08 07:16:17 -0500

    Convert db mathml data to latex

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MathMLToLatex
 Convert MathML to Latex
 
-Before run the project, intsall lxml, psycopg2.
+Before run the project, intsall lxml, psycopg2, BeautifulSoup
 
 We have used the code from: https://github.com/oerpub/mathconverter
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 from lxml import etree
 from lxml.builder import unicode
 from psycopg2 import connect
+from bs4 import BeautifulSoup
 
 SERVERNAME = 'localhost'
 USERNAME = 'postgres'
-PASSWORD = ''
+PASSWORD = 'postgres'
 PORT = '5432'
 
 # district name is the database name.
@@ -31,33 +32,58 @@ def mathml2latex_yarosh(equation):
     transform = etree.XSLT(xslt)
     newdom = transform(dom)
     return unicode(newdom)
-    
 
-if __name__ == '__main__':
+
+def converter():
     try:
         conn = newConnection(districts[0]['district_name'])
         cur = conn.cursor()
-
-        # execute a statement
-        print('PostgreSQL database version:')
-        # cur.execute('SELECT version()')
-        cur.execute('SELECT version()')
+        # cur.execute('SELECT question_content FROM edg.asmt_question OFFSET 0 LIMIT 1')
+        # cur.execute('SELECT question_content FROM edg.asmt_question WHERE id = 6050457')
+        cur.execute('SELECT question_content FROM edg.asmt_question WHERE id = 2073509')
 
         # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
+        content = cur.fetchone()
+        # print(content)
+        
+        for item in content[0]:
+            # print(item)
+            if item != 'answer' and item != 'choicesArr':
+                # print(content[0][item])
+                # print (content[0][item])
+                soup = BeautifulSoup(content[0][item], 'html.parser')
+                maths = soup.find_all('math')
+                
+                index = 0
+                while index < len(maths):
+                    mathml = str(maths[index])
+                    print('\nMathml: ' + mathml + '\n')
+                    if mathml[:7] == '<math> ':
+                        mathml = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"> " + mathml[7:]
+                        # print(mathml)
+                    latex = mathml2latex_yarosh(mathml)
+                    latex = latex.replace("$", "")
+                    print('Latex: ' + latex + '\n')
+                    index += 1
+                    # create a schema 'temp' and create table 'question_update'
+                    # Save into this, id / original question / updated content
 
+        
+        # mathml = """<prompt  xmlns:m=\"http://www.w3.org/1998/Math/MathML\" ><p>Graph the solution set to <span class=\"rsc_eqtn\"><math>   <mn>5</mn>   <mi>x</mi>   <mo>&#8722;<!-- &#8722; --></mo>   <mn>3</mn>   <mi>y</mi>   <mo>&lt;</mo>   <mo>&#8722;</mo>   <mn>15</mn> </math></span>.</p>  <p></p></prompt>"""
+        # if mathml[:7] == '<math> ':
+        #     mathml = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"> " + mathml[7:]
+        # print(mathml)
         # close the communication with the PostgreSQL
         cur.close()
 
 
-        mathml = input('Enter MathML: ')
-        if mathml[:7] == '<math> ':
-            mathml = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"> " + mathml[7:]
+        # mathml = input('Enter MathML: ')
+        # if mathml[:7] == '<math> ':
+        #     mathml = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"> " + mathml[7:]
 
-        latex = mathml2latex_yarosh(mathml)
-        latex = latex.replace("$", "")
-        print('Latex: ' + latex)
+        # latex = mathml2latex_yarosh(mathml)
+        # latex = latex.replace("$", "")
+        # print('Latex: ' + latex)
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     except:
@@ -66,4 +92,8 @@ if __name__ == '__main__':
         if conn is not None:
             conn.close()
             print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    converter()
     
