b6a9b8a0cb77e166392af73e5204656b921725f7
MahdiRe <mahdhi.dts@gmail.com>
2022-07-08 00:33:22 -0500

    DB connection added

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # MathMLToLatex
 Convert MathML to Latex
 
+Before run the project, intsall lxml, psycopg2.
+
 We have used the code from: https://github.com/oerpub/mathconverter
--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import sys
 import os
 from lxml import etree
 from lxml.builder import unicode
+from psycopg2 import connect
+
+SERVERNAME = 'localhost'
+USERNAME = 'postgres'
+PASSWORD = ''
+PORT = '5432'
+
+# district name is the database name.
+districts = [{"db_name": "demo"}]
+
+
+def newConnection(district):
+    try:
+        conn = connect("host=" + SERVERNAME + " port=" + PORT + " dbname=" +
+                       district + " user=" + USERNAME + " password=" + PASSWORD)
+        return conn
+    except Exception as e:
+        raise Exception(e)
 
 
 def mathml2latex_yarosh(equation):
@@ -13,10 +31,26 @@ def mathml2latex_yarosh(equation):
     transform = etree.XSLT(xslt)
     newdom = transform(dom)
     return unicode(newdom)
-
+    
 
 if __name__ == '__main__':
     try:
+        conn = newConnection(districts[0]['district_name'])
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        # cur.execute('SELECT version()')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+
+
         mathml = input('Enter MathML: ')
         if mathml[:7] == '<math> ':
             mathml = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"> " + mathml[7:]
@@ -24,5 +58,12 @@ if __name__ == '__main__':
         latex = mathml2latex_yarosh(mathml)
         latex = latex.replace("$", "")
         print('Latex: ' + latex)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
     except:
         print("Please enter a valid MathML expression!")
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+    
